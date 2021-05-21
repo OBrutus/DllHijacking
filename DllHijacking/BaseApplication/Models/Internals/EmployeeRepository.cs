724b1782202fc09f82004d468cc85e28f9760df9
Aniket Vaishnav <37221337+OBrutus@users.noreply.github.com>
2021-05-22 01:32:38 +0530

    Initial Commit  Added Files via upload

---
+++ DllHijacking/BaseApplication/Models/Internals/EmployeeRepository.cs
@@ -0,0 +1,40 @@
+﻿using LegitClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BaseApplication.Models.Internals
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private List<Employee> emplyeeRegistar;
+        public EmployeeRepository()
+        {
+            emplyeeRegistar = new List<Employee>();
+            new DataProvider();
+            emplyeeRegistar.AddRange(new[] {
+                new Employee() { Name = "Ram"},
+                new Employee() { Name = "Shyam"},
+                new Employee() { Name = "Milind"},
+                new Employee() { Name = "Raj"}
+            });
+        }
+
+        public ICollection<Employee> GetAllEmployees()
+        {
+            return emplyeeRegistar;
+        }
+
+        public Employee GetEmployee(string name)
+        {
+            name = name.ToLower();
+            return emplyeeRegistar.FirstOrDefault<Employee>(emp => emp.Name.ToLower() == name);
+        }
+
+        public bool RemoveEmployee(string name)
+        {
+            return  emplyeeRegistar.RemoveAll(emp => emp.Name == name) > 0;
+        }
+    }
+}
---
+++ DllHijacking/BaseApplication/Models/Internals/IEmployeeRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BaseApplication.Models.Internals
+{
+    public interface IEmployeeRepository
+    {
+        public Employee GetEmployee(string name);
+        public bool RemoveEmployee(string name);
+        public ICollection<Employee> GetAllEmployees();
+    }
+}
